46c1c68906736ce333f6634865c613e6b4174960
Mohammed Gamal <mg.moh.gamal@gmail.com>
2021-03-16 20:26:52 +0200

    [injectionsplugin] Initial commit: Injections plugin.
    	- A simple plugin to make the injections easier.
    	- Injections in  the plugin can be added to a list to resend
    	  them later if needed with the ability to change the details of
    	  the injection.
    	- TODO: Save/restore the injections from a file.
    	- TODO: Injection macros.

---
+++ plugin/injectionsplugin/injectionsplugin.h
@@ -0,0 +1,94 @@
+/**
+ * @licence app begin@
+ * Copyright (C) 2011-2012  BMW AG
+ *
+ * This file is part of GENIVI Project Dlt Viewer.
+ *
+ * Contributions are licensed to the GENIVI Alliance under one or more
+ * Contribution License Agreements.
+ *
+ * \copyright
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a  copy of the MPL was not distributed with
+ * this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * \file injectionsplugin.h
+ * For further information see https://github.com/sudomgamal/dlt-viewer
+ * @licence end@
+ */
+
+#ifndef INJECTIONSPLUGIN_H
+#define INJECTIONSPLUGIN_H
+
+#include <QObject>
+#include "plugininterface.h"
+#include "form.h"
+
+#define INJECTIONS_PLUGIN_VERSION "1.0.0"
+
+class InjectionsPlugin : public QObject, QDLTPluginInterface, QDltPluginViewerInterface, QDltPluginControlInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(QDLTPluginInterface)
+    Q_INTERFACES(QDltPluginViewerInterface)
+    Q_INTERFACES(QDltPluginControlInterface)
+#ifdef QT5
+    Q_PLUGIN_METADATA(IID "injectionsPlugin")
+#endif
+
+public:
+    InjectionsPlugin();
+    ~InjectionsPlugin();
+
+    /* QDLTPluginInterface interface */
+    QString name();
+    QString pluginVersion();
+    QString pluginInterfaceVersion();
+    QString description();
+    QString error();
+    bool loadConfig(QString filename);
+    bool saveConfig(QString filename);
+    QStringList infoConfig();
+
+    /* QDltPluginViewerInterface */
+    QWidget* initViewer();
+    void initFileStart(QDltFile *file);
+    void initFileFinish();
+    void initMsg(int index, QDltMsg &msg);
+    void initMsgDecoded(int index, QDltMsg &msg);
+    void updateFileStart();
+    void updateMsg(int index, QDltMsg &msg);
+    void updateMsgDecoded(int index, QDltMsg &msg);
+    void updateFileFinish();
+    void selectedIdxMsg(int index, QDltMsg &msg);
+    void selectedIdxMsgDecoded(int index, QDltMsg &msg);
+
+    /* QDltPluginControlInterface */
+    bool initControl(QDltControl *control);
+    bool initConnections(QStringList list);
+    bool controlMsg(int index, QDltMsg &msg);
+    bool stateChanged(int index, QDltConnection::QDltConnectionState connectionState,QString hostname);
+    bool autoscrollStateChanged(bool enabled);
+    void initMessageDecoder(QDltMessageDecoder* pMessageDecoder);
+    void initMainTableView(QTableView* pTableView);
+    void configurationChanged();
+
+    /* internal variables */
+    Injections::Form *form;
+    int counterMessages;
+    int counterNonVerboseMessages;
+    int counterVerboseMessages;
+
+    void show(bool value);
+
+    void updateCounters(int start,int end);
+
+    QDltControl *dltControl;
+
+private:
+    QDltFile *dltFile;
+    QString errorText;
+    QStringList *ecuList;
+};
+
+#endif // INJECTIONSPLUGIN_H
